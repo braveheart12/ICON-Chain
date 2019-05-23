dcb37aee24c749fefe931502a54521820862bcd5
Seongjun Ji <metalg0su@gmail.com>
2019-05-23 14:09:27 +0900

    Delete unusedfiles

--- loopchain/rest_server/__init__.py
+++ loopchain/rest_server/__init__.py
@@ -12,7 +12,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """A package for restful API server"""
-
-from .peer_service_stub import *
-from .rest_property import *
-from .rest_server_rs import *
